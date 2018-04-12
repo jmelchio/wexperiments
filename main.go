78b6ab1c5072d912b42f2a3e442226de9c077efb
Joris Melchior <joris.melchior@gmail.com>
2018-04-11 22:15:09 -0400

    Starting the use of templates

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func main() {
+	templates := populateTemplates()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		requestedFile := req.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
 	http.HandleFunc("/goapp", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
 		writer.Header().Add("Content-Type", "text/plain")
@@ -16,7 +35,15 @@ func main() {
 	})
 
 	fileServer := http.FileServer(http.Dir("./public"))
-	http.Handle("/public/", http.StripPrefix("/public/", fileServer))
+	http.Handle("/img/", fileServer)
+	http.Handle("/css/", fileServer)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
